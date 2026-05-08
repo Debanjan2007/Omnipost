3d4b33d66c19f5dbdbf7358cd6356ee0b699fd98
Debanjan Das <microsoft2420@gmail.com>
2026-05-08 21:19:55 +0530

    update the roouter with proper dashboard path

--- app/web/omnipost-web-ui/src/main.jsx
+++ app/web/omnipost-web-ui/src/main.jsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import Layout from "./Layout.jsx";
-import { CreatePost } from "./components/index.jsx";
+import { CreatePost , Accounts , History } from "./components/index.jsx";
+import DashboardPage from "./components/HeroSection/Dashboard/DashboardPage.jsx";
+
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<Layout />}>
+            <Route path='dashboard' element={<DashboardPage />} />
             <Route path='create' element={<CreatePost />}/>
-            <Route path='accounts'/>
-            <Route path='history'/>
+            <Route path='accounts' element={<Accounts />}/>
+            <Route path='history' element={<History />}/>
             <Route path='settings'/>
         </Route>
     )
