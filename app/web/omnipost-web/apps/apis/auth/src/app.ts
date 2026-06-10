0fef9afabf8192a92164f9b4a7f0a679c06326a8
Debanjan Das <microsoft2420@gmail.com>
2026-06-11 00:01:05 +0530

    update auth dir

--- app/web/omnipost-web/apps/apis/auth/src/app.ts
+++ app/web/omnipost-web/apps/apis/auth/src/app.ts
@@ -1,7 +1,10 @@
 ﻿import express from 'express';
+import { globalErrHandler } from './utils/globalError'
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// here come other routes
+app.use(globalErrHandler) // last middleware
 
 export default app;
