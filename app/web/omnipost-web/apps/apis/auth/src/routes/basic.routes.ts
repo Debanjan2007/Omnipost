a1cb5865fa4931d0091ea438a5e34ef57cbebae6
Debanjan Das <microsoft2420@gmail.com>
2026-06-14 21:22:53 +0530

    Updated things

--- app/web/omnipost-web/apps/apis/auth/src/routes/basic.routes.ts
+++ app/web/omnipost-web/apps/apis/auth/src/routes/basic.routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express'
+import { authUrlReturn } from '../controller/auth.controller'
 
 const router = Router()
+
+// @ts-ignores
+router.get('/signup', authUrlReturn)
 
 export default router
