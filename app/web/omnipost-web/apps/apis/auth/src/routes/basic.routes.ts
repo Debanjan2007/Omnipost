ff95215e61bd9cf226bacd3b56fcda5b675e7b79
Debanjan Das <microsoft2420@gmail.com>
2026-06-15 20:41:44 +0530

    Add POST /signup route with middleware

--- app/web/omnipost-web/apps/apis/auth/src/routes/basic.routes.ts
+++ app/web/omnipost-web/apps/apis/auth/src/routes/basic.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { authUrlReturn } from '../controller/auth.controller'
+import { authUrlReturn , signUp } from '../controller/auth.controller'
+import { extractUserData } from '../middleware/ExtractReqData.middleware'
 
 const router = Router()
 
-// @ts-ignores
 router.get('/authUrl', authUrlReturn)
+router.post('/signup', extractUserData , signUp)
 
 export default router
