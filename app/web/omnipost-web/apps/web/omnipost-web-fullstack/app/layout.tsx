2265ef514fa0dae7903f8dd0b19421dfca5eb62a
Debanjan Das <microsoft2420@gmail.com>
2026-07-06 17:54:01 +0530

    Refacored the navbar

--- app/web/omnipost-web/apps/web/omnipost-web-fullstack/app/layout.tsx
+++ app/web/omnipost-web/apps/web/omnipost-web-fullstack/app/layout.tsx
@@ -1,12 +1,6 @@
 import type {Metadata} from "next";
 import {Geist_Mono, Poppins} from "next/font/google";
 import "./globals.css";
-import Image from "next/image";
-import Link from "next/link";
-import {RippleButton} from "@/components/ui/ripple-button"
-import {InteractiveHoverButton} from "@/components/ui/interactive-hover-button"
-import {AnimatedThemeToggler} from "@/components/ui/animated-theme-toggler"
-import { MobileNav } from "./Components/Mobile-nav"
 
 const poppins = Poppins({
     variable: "--font-poppins",
@@ -35,52 +29,8 @@ export default function RootLayout({
             className={`${poppins.variable} ${geistMono.variable} h-full antialiased`}
         >
         <body className="font-sans min-h-full flex flex-col">
-        <div
-            className="hidden layout-header md:p-8 lg:px-16 md:flex items-center justify-between  md:h-18.5 lg:h-21 w-screen">
-            <div className="logo flex items-center gap-2.5 cursor-pointer">
-                <Image src="/logo.png" alt="omnipost-logo" width={44} height={44}/>
-                <span className="text-2xl font-bold">OmniPost</span>
-            </div>
-            <div className="flex items-center lg:gap-6 md:gap-4">
-                <button
-                    className="text-sm cursor-pointer font-medium text-text-secondary hover:text-text-primary transition">
-                    Solution
-                </button>
-                <button
-                    className="text-sm cursor-pointer font-medium text-text-secondary hover:text-text-primary transition">
-                    Features
-                </button>
-                <button
-                    className="text-sm cursor-pointer font-medium text-text-secondary hover:text-text-primary transition">
-                    Pricing
-                </button>
-                <button
-                    className="text-sm cursor-pointer font-medium text-text-secondary hover:text-text-primary transition">
-                    About
-                </button>
-                <button
-                    className="text-sm cursor-pointer font-medium text-text-secondary hover:text-text-primary transition">
-                    Contact
-                </button>
-            </div>
-            <div className="controll-buttons flex items-center gap-4">
-                <AnimatedThemeToggler/>
-                <RippleButton>
-                    <Link href="/login">Login</Link>
-                </RippleButton>
-                <InteractiveHoverButton>
-                    <Link href="/signup">
-                        Sign up
-                    </Link>
-                </InteractiveHoverButton>
-            </div>
-        </div>
-        <div
-            className="layout-header-mobile h-16 w-screen flex items-center justify-between px-4 md:hidden lg:hidden">
-            <MobileNav />
-        </div>
         {children}
         </body>
         </html>
-);
+    );
 }
--- app/web/omnipost-web/apps/web/omnipost-web-fullstack/app/page.tsx
+++ app/web/omnipost-web/apps/web/omnipost-web-fullstack/app/page.tsx
@@ -1,8 +1,60 @@
 import Image from "next/image";
+import {MobileNav} from "./Components/Mobile-nav"
+import Link from "next/link";
+import {RippleButton} from "@/components/ui/ripple-button"
+import {InteractiveHoverButton} from "@/components/ui/interactive-hover-button"
+import {AnimatedThemeToggler} from "@/components/ui/animated-theme-toggler"
+
 
 export default function Home() {
-  return (
-      <div className="Dashboard h-full w-full bg-red-100 md:h-full">
-      </div>
-  );
+    return (
+        <div className="landing-page h-screen w-screen">
+            <nav>
+                <div
+                    className="hidden layout-header md:p-8 lg:px-16 md:flex items-center justify-between  md:h-18.5 lg:h-21 w-screen">
+                    <div className="logo flex items-center gap-2.5 cursor-pointer">
+                        <Image src="/logo.png" alt="omnipost-logo" width={44} height={44}/>
+                        <span className="text-2xl font-bold">OmniPost</span>
+                    </div>
+                    <div className="flex items-center lg:gap-6 md:gap-4">
+                        <button
+                            className="text-sm cursor-pointer font-medium text-text-secondary hover:text-text-primary transition">
+                            Solution
+                        </button>
+                        <button
+                            className="text-sm cursor-pointer font-medium text-text-secondary hover:text-text-primary transition">
+                            Features
+                        </button>
+                        <button
+                            className="text-sm cursor-pointer font-medium text-text-secondary hover:text-text-primary transition">
+                            Pricing
+                        </button>
+                        <button
+                            className="text-sm cursor-pointer font-medium text-text-secondary hover:text-text-primary transition">
+                            About
+                        </button>
+                        <button
+                            className="text-sm cursor-pointer font-medium text-text-secondary hover:text-text-primary transition">
+                            Contact
+                        </button>
+                    </div>
+                    <div className="controll-buttons flex items-center gap-4">
+                        <AnimatedThemeToggler/>
+                        <RippleButton>
+                            <Link href="/login">Login</Link>
+                        </RippleButton>
+                        <InteractiveHoverButton>
+                            <Link href="/signup">
+                                Sign up
+                            </Link>
+                        </InteractiveHoverButton>
+                    </div>
+                </div>
+                <div
+                    className="layout-header-mobile h-16 w-screen flex items-center justify-between px-4 md:hidden lg:hidden">
+                    <MobileNav/>
+                </div>
+            </nav>
+        </div>
+    );
 }
