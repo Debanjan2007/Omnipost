d03b581de6e78dfdc23d48f1d4b0af445af4cd2a
Debanjan Das <microsoft2420@gmail.com>
2026-07-06 17:45:49 +0530

    Refactored the file

--- app/web/omnipost-web/apps/web/omnipost-web-fullstack/app/layout.tsx
+++ app/web/omnipost-web/apps/web/omnipost-web-fullstack/app/layout.tsx
@@ -6,16 +6,7 @@ import Link from "next/link";
 import {RippleButton} from "@/components/ui/ripple-button"
 import {InteractiveHoverButton} from "@/components/ui/interactive-hover-button"
 import {AnimatedThemeToggler} from "@/components/ui/animated-theme-toggler"
-import {
-    Sheet,
-    SheetClose,
-    SheetContent,
-    SheetFooter,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-} from "@/components/ui/sheet"
-import {Menu} from "lucide-react"
+import { MobileNav } from "./Components/Mobile-nav"
 
 const poppins = Poppins({
     variable: "--font-poppins",
@@ -85,65 +76,8 @@ export default function RootLayout({
             </div>
         </div>
         <div
-            className="layout-header-mobile h-16 w-screen flex items-center justify-between justify-between px-4 md:hidden lg:hidden">
-            <Sheet>
-                <SheetTrigger><Menu/></SheetTrigger>
-                <SheetContent>
-                    <SheetHeader>
-                        <SheetTitle>
-                            <div className="logo flex items-center gap-2.5 cursor-pointer">
-                                <Image src="/logo.png" alt="omnipost-logo" width={32} height={32}/>
-                                <span className="text-2xl font-bold">OmniPost</span>
-                            </div>
-                        </SheetTitle>
-                    </SheetHeader>
-                    <div className="w-full flex flex-col gap-4 pr-2">
-                        <ul className="w-full flex flex-col gap-4 p-4">
-                            <li>
-                                <button
-                                    className="text-sm cursor-pointer font-medium text-text-secondary hover:text-text-primary transition">
-                                    Solution
-                                </button>
-                            </li>
-                            <li>
-                                <button
-                                    className="text-sm cursor-pointer font-medium text-text-secondary hover:text-text-primary transition">
-                                    Features
-                                </button>
-                            </li>
-                            <li>
-                                <button
-                                    className="text-sm cursor-pointer font-medium text-text-secondary hover:text-text-primary transition">
-                                    Pricing
-                                </button>
-                            </li>
-                            <li>
-                                <button
-                                    className="text-sm cursor-pointer font-medium text-text-secondary hover:text-text-primary transition">
-                                    About
-                                </button>
-                            </li>
-                            <li>
-                                <button
-                                    className="text-sm cursor-pointer font-medium text-text-secondary hover:text-text-primary transition">
-                                    Contact
-                                </button>
-                            </li>
-                        </ul>
-                    </div>
-                </SheetContent>
-            </Sheet>
-            <div className="controll-buttons flex items-center gap-4">
-                <RippleButton>
-                    <Link href="/login">Login</Link>
-                </RippleButton>
-                <InteractiveHoverButton>
-                    <Link href="/signup">
-                        Sign up
-                    </Link>
-                </InteractiveHoverButton>
-                <AnimatedThemeToggler/>
-            </div>
+            className="layout-header-mobile h-16 w-screen flex items-center justify-between px-4 md:hidden lg:hidden">
+            <MobileNav />
         </div>
         {children}
         </body>
