b9b0227cbe8bcb3b32147cae97e40ff6167c1fa3
Debanjan Das <microsoft2420@gmail.com>
2026-08-02 09:48:28 +0530

    feat(s3): implement S3 controller with bucket operations and export utilities

---
+++ app/web/omnipost-web/shared/s3/controller/index.ts
@@ -0,0 +1,54 @@
+import { s3 } from './connect.s3.js'
+import { CreateBucketCommand , PutObjectCommand , GetObjectCommand , DeleteObjectCommand } from '@aws-sdk/client-s3'
+
+const createBucket = async (bucketName: string) => {
+    const command = new CreateBucketCommand({ Bucket: bucketName })
+    await s3.send(command)
+}
+
+const puObject = async (bucket: string , key: string , body: any) => {
+    try{
+        const command = new PutObjectCommand({
+            Bucket: bucket,
+            Key: key,
+            Body: body,
+        })
+        await s3.send(command)
+    }catch (e: any) {{
+        console.log(e)
+        throw new Error(e.message)
+    }}
+}
+
+const getObject = async (bucket: string , key: string) => {
+    try{
+        const command = new GetObjectCommand({
+            Bucket: bucket,
+            Key: key,
+        })
+        return await s3.send(command)
+    }catch (e: any){
+        console.log(e)
+        throw new Error(e.message)
+    }
+}
+
+const delObject = async (bucket: string , key: string) => {
+    try{
+        const command = new DeleteObjectCommand({
+            Bucket: bucket,
+            Key: key,
+        })
+        await s3.send(command)
+    }catch (e: any){
+        console.log(e)
+        throw new Error(e.message)
+    }
+}
+
+export {
+    createBucket,
+    puObject,
+    getObject
+}
+export * from './utils/Bucketexists.utils.s3.js'
