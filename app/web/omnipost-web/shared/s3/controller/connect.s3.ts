e06ceb5ef5cee358f8762143f740e28075697106
Debanjan Das <microsoft2420@gmail.com>
2026-08-02 09:35:28 +0530

    feat(s3): implement S3 connection and bucket existence check utilities

---
+++ app/web/omnipost-web/shared/s3/controller/connect.s3.ts
@@ -0,0 +1,22 @@
+import {S3Client} from '@aws-sdk/client-s3'
+
+
+let s3: any = undefined;
+const connectS3 = async () => {
+    s3 = new S3Client({
+        endpoint: process.env.MINIO_ENDPOINT as string,
+        region: process.env.MINIO_REGION as string,
+        credentials: {
+            accessKeyId: process.env.MINIO_ROOT_USERNAME as string,
+            secretAccessKey: process.env.MINIO_ROOT_PASSWORD as string,
+        },
+        forcePathStyle: true,
+        requestChecksumCalculation: "WHEN_REQUIRED",
+    })
+}
+
+
+export {
+    connectS3,
+    s3
+}
