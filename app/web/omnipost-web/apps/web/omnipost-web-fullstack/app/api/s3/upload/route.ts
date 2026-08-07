4dd224a7f3d6eb0210525e992c6b9ae188bf1ba4
Debanjan Das <microsoft2420@gmail.com>
2026-08-07 11:43:08 +0530

    feat: enhance file upload by using UniqueKey for generated filenames and improve error response

--- app/web/omnipost-web/apps/web/omnipost-web-fullstack/app/api/s3/upload/route.ts
+++ app/web/omnipost-web/apps/web/omnipost-web-fullstack/app/api/s3/upload/route.ts
@@ -1,4 +1,4 @@
-import {connectS3, createBucket, putObject} from '@repo/s3'
+import {connectS3, createBucket, putObject, UniqueKey} from '@repo/s3'
 import {NextRequest, NextResponse} from 'next/server';
 import {TOAST_EVENTS} from "@/lib/toasts";
 
@@ -17,19 +17,27 @@ export async function POST(req: NextRequest) {
         if (!fileTypes.includes(file.type)) {
             return Error("File type not supported")
         }
-        const object = await putObject(process.env.S3_BUCKET_NAME as string, file.name, fileBuffer)
+        const key = UniqueKey(file.name)
+        const object = await putObject(process.env.S3_BUCKET_NAME as string, key, fileBuffer)
         console.log(object)
         if (!object.success) {
-            return Error("Failed to upload file")
+            return NextResponse.json(
+                {
+                    success: false,
+                    message: "Failed to upload file"
+                },
+                {status: 500}
+            )
         }
-        return {
-            status: 200,
-            success: true,
-            data: {
-                key: file.name,
-            }
-        }
-    }catch (e){
+        return NextResponse.json({
+                success: true,
+                data: {
+                    key
+                },
+            },
+            {status: 200}
+        )
+    } catch (e) {
         console.log(e)
         return NextResponse.redirect(
             new URL(`/dashboard?toast=${TOAST_EVENTS.upload_failed}`, req.url).toString(),
